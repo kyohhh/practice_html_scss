235e6681f8484306e7f18069eacbd8390073ba22
kyohhh <khnm0322@gmail.com>
2021-07-07 23:45:03 +0900

    修正_1

--- functions.php
+++ functions.php
@@ -11,14 +11,14 @@ function my_setup() {
     'comment-list',
     'gallery',
     'caption',
-  ));
+  ) );
 }
-add_action( 'after_setup_theme', 'my_setup' )
+add_action( 'after_setup_theme', 'my_setup' );
 
 //CSS / Javascriptの読み込み
 
 function my_script_init() {
-  wp_enqueue_style( 'style_name', get_template_directory_url() . './style.css', array(), '1.0.0', 'all' );
-  wp_enqueue_style( 'style_name', get_template_directory_url() . './css/reset.css', array(), '1.0.0', 'all' );
-  }
-  aad_action( 'wp_enqueue_scripts', 'my_script_init' );
+  wp_enqueue_style( 'style-name', get_template_directory_uri() . './style.css', array(), '1.0.0', 'all' );
+  wp_enqueue_style( 'style-name', get_template_directory_uri() . './css/reset.css', array(), '1.0.0', 'all' );
+}
+add_action( 'wp_enqueue_scripts', 'my_script_init' );
--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php wp_title( '|', true, 'right' ); //ページタイトルを出力 ?><?php bloginfo('name'); //サイト名を表示 ?></title>
-    <link rel="stylesheet" href="<?php echo get_stylesheet_url(); //使用中テーマのスタイルシートURLを出力 ?>">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); //使用中テーマのスタイルシートURLを出力 ?>">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); //ピングバックURLを出力 ?>">
     <?php wp_head(); //wp_headはテーマの</head>タグ直前に必ず挿入 ?>
 </head>
@@ -14,7 +14,7 @@
 <body <?php body_class(); //bodyタグにページの種類に応じたクラス名を与える ?>>
     <!-- ヘッダー -->
     <header class="header">
-        <img class="header-logo" src="<?php echo get_template_directory_url(); //使用中のテーマURLに置き換わるb?>./image/logo.png" alt="コーディングスクールのロゴ">
+        <img class="header-logo" src="<?php echo get_template_directory_uri(); //使用中のテーマURLに置き換わるb?>./image/logo.png" alt="コーディングスクールのロゴ">
         <div class="header-nav">
             <nav class="nav">
                 <ul class="nav-list">
